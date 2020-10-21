284be648e3036807b0cee98cd5752cb45867f9ff
aj8457mau <62740785+aj8457mau@users.noreply.github.com>
2020-10-21 11:07:25 +0200

    bug fix

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -7,7 +7,7 @@ export default class CreateExercise extends Component {
   constructor(props) {
       super(props);
 
-      this.onChangeUsername = this.onChangeUsername.bind(this);
+      this.onChangeUsername = this.onChangeUsername.bind(this); //binding
       this.onChangeDescription= this.onChangeDescription.bind(this);
       this.onChangeDuration = this.onChangeDuration.bind(this);
       this.onChangeDate = this.onChangeDate.bind(this);
